07e3543974dce4d8285e7154d71770bc490a46fe
Jim Tseng <jimtseng@Jims-MBP.attlocal.net>
2023-05-19 11:21:50 -0700

    Code Revert

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View className="flex justify-center items-center">
+      <Text className="text-red-800 py-14 text-2xl">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
